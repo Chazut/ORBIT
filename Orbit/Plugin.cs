b411d8333d2df20e73449746a67927b0da8dd422
Jagut <jagut.jeremy@gmail.com>
2026-06-14 12:21:42 +0200

    Bump version to 1.1.0

--- Orbit/Plugin.cs
+++ Orbit/Plugin.cs
@@ -37,7 +37,7 @@ public class Plugin : BaseUnityPlugin
 {
     public const string PluginGuid = "com.chazut.orbit";
     public const string PluginName = "ORBIT";
-    public const string OrbitVersion = "1.0.0";
+    public const string OrbitVersion = "1.1.0";
 
     public static ManualLogSource LogSource;
 
